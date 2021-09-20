9a414bbd4d6702943a1e8bc0bc5981feffd619b7
lokesh020 <lokeshjangid020@gmail.com>
2021-09-20 19:39:02 +0530

    minor fix

--- srcImagePicker/TestImagePicker.js
+++ srcImagePicker/TestImagePicker.js
@@ -20,7 +20,7 @@ const TestImagePicker = (params) =>  {
             if (response.didCancel) {
                 
             } else if (response.errorCode) {
-                console.log('ImagePicker Error: >>>', response.errorMessage);
+                console.log('ImagePicker Error Happend: >>>', response.errorMessage);
                 this.handleError(response.errorCode)
                 reject(response.errorCode)
             } else {
